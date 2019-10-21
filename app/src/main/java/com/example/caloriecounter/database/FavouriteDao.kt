50f5874adffe9e33fbece7e9dc96fa3621cbad24
Ewelina Moniuszko-Szymanska <ewelina@geneo.co.uk>
2019-10-21 15:23:16 +0100

    added repository methods for search, adding, gettingBYId,deletingById, editing Favourites

--- app/src/main/java/com/example/caloriecounter/database/FavouriteDao.kt
+++ app/src/main/java/com/example/caloriecounter/database/FavouriteDao.kt
@@ -4,16 +4,19 @@ import androidx.room.*
 
 @Dao
 interface FavouriteDao {
-    @Query("SELECT * FROM favourites where name = :name and type=:type")
-    fun getByNameAndType(name: String, type: String): Favourite
+    @Query("SELECT * FROM favourites where CONTAINS(name, :name) and type=:type")
+    fun getByNameAndType(name: String, type: String): List<Favourite>
 
     @Query("SELECT * FROM favourites where id = :id")
     fun getById(id: Int?): Favourite
 
-    @Insert()
-    fun insert(favourite: Favourite):Boolean
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    fun insert(favourite: Favourite)
 
     @Query("DELETE FROM favourites WHERE id = :id")
     fun deleteByFavouriteId(id: Int?)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun edit(newFavourite: Favourite)
 
 }
