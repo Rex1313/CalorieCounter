4cdabe687e97d9a215314d9179b087d1f0fb3b66
Ewelina Moniuszko-Szymanska <ewelina@geneo.co.uk>
2019-11-14 15:27:06 +0000

    wip

--- app/src/main/java/com/example/caloriecounter/SplashScreenFragment.kt
+++ app/src/main/java/com/example/caloriecounter/SplashScreenFragment.kt
@@ -1,5 +1,6 @@
 package com.example.caloriecounter
 
+import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -11,6 +12,14 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.example.caloriecounter.dayview.DayFragment
 import kotlinx.android.synthetic.main.splash_screen_fragment.*
+import androidx.core.view.ViewCompat.setScaleX
+import android.R.attr.y
+import android.R.attr.x
+import android.graphics.SurfaceTexture
+import org.robolectric.shadows.ShadowDisplay.getDefaultDisplay
+import android.util.DisplayMetrics
+
+
 
 
 class SplashScreenFragment : Fragment() {
@@ -24,11 +33,11 @@ class SplashScreenFragment : Fragment() {
     ): View? {
         viewmodel = ViewModelProviders.of(this).get(SplashScreenFragmentViewModel::class.java)
         context?.let {
-            if (viewmodel.getWelcomeScreenViewed(it)) {
-                findNavController().navigate(R.id.action_splashScreenFragment_to_daysFragment)
-            }else{
-                viewmodel.setWelcomeScreenViewed(it)
-            }
+//            if (viewmodel.getWelcomeScreenViewed(it)) {
+//                findNavController().navigate(R.id.action_splashScreenFragment_to_daysFragment)
+//            }else{
+//                viewmodel.setWelcomeScreenViewed(it)
+//            }
         }
        return inflater.inflate(R.layout.splash_screen_fragment, container, false)
     }
@@ -37,9 +46,9 @@ class SplashScreenFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         (activity as AppCompatActivity).supportActionBar?.hide()
-
-        video_view_welcome.setVideoURI(Uri.parse("android.resource://"+ (activity as AppCompatActivity).getPackageName() + "/raw/berry2"))
-        video_view_welcome.start()
+//
+//        video_view_welcome.setVideoURI(Uri.parse("android.resource://"+ (activity as AppCompatActivity).getPackageName() + "/raw/berry2"))
+//        video_view_welcome.start()
 
         val dayFragment = DaysFragment.newInstance()
         button_welcome.setOnClickListener {
@@ -47,7 +56,48 @@ class SplashScreenFragment : Fragment() {
         }
 
     }
+    video_view_welcome.setSurfaceTextureListener((){
 
+    })
+//    video_view_welcome.setSurfaceTextureListener(new SurfaceTextureListener() {
+//        @Override
+//        public void onSurfaceTextureAvailable(
+//            SurfaceTexture surface,
+//            int width, int height) {
+//            FullScreenActivity.this.mSurface = surface;
+//
+//        }
+//private fun scaleVideo(mPlayer: MediaPlayer) {
+//
+//    val videoParams = mTextureView
+//        .getLayoutParams() as LayoutParams
+//    val dm = DisplayMetrics()
+//    this@FullScreenActivity.getWindowManager().getDefaultDisplay()
+//        .getMetrics(dm)
+//
+//    val height = dm.heightPixels
+//    val width = dm.widthPixels
+//    val videoHeight = mPlayer.videoHeight
+//    val videoWidth = mPlayer.videoWidth
+//    var hRatio = 1.0
+//
+//    hRatio = height * 1.0 / videoHeight / (width * 1.0 / videoWidth)
+//    videoParams.x = (if (hRatio <= 1)
+//        0
+//    else
+//        Math.round(-(hRatio - 1) / 2 * width)).toInt()
+//    videoParams.y = (if (hRatio >= 1)
+//        0
+//    else
+//        Math
+//            .round((-1 / hRatio + 1) / 2 * height)).toInt()
+//    videoParams.width = width - videoParams.x - videoParams.x
+//    videoParams.height = height - videoParams.y - videoParams.y
+//    mTextureView.setScaleX(1.00001f)//<-- this line enables smoothing of the picture in TextureView.
+//    mTextureView.requestLayout()
+//    mTextureView.invalidate()
+//
+//}
     companion object {
         fun newInstance() = SplashScreenFragment()
     }
